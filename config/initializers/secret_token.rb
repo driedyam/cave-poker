59bc28071c5e07a1e48eb865bc2ea89736b49451
Chris Barretto <chrisbarretto@chriss-mbp.attlocal.net>
2014-11-11 18:21:27 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CavePoker::Application.config.secret_token = '4af508bfbea2cbffa717542ebdefbd12e00ef620b4f366ad7e349b2eea297da9bbaec7f414eb4966a8f9d17604806abf52dc3c60b3f775dda40a18b135ce0dce'
